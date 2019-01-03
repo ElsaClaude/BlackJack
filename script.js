29daa8c183cd3a27254ae5e63538cab5fb241800
claudeelsa <claudeelsa@outlook.fr>
2019-01-03 11:25:19 +0100

    Valeurs as

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ function setupListeners() {
   // let scoreplayer=0;
   // let scoredealer=0;
   cardlist();
-  scoredealer=deal(scoredealer,"Dealer's","dealerarea");
+  let tmp3=[];
+  tmp3=deal(scoredealer,"Dealer's","dealerarea",as_dealer);
+  scoredealer=tmp3[0];
+  as_dealer=tmp3[1];
   var carddeal = document.getElementById("cardbutton");
   carddeal.addEventListener("click",player);
   var staybutton = document.getElementById("staybutton");
@@ -16,20 +19,37 @@ function setupListeners() {
 var cardlist=function() {
   for (let i=1; i<=nbcard; i++){
     let name_image="./Images/"+i+".BMP";
-    if (i%13<=10 && i%13!=0) {
+    if (i%13<=10 && i%13!=0 && i%13!=1) {
       j=i%13;
     }
     else if (i%13>10 || i%13==0) {
       j=10;
     }
+    else if (i%13==1) {
+      j=11;
+    }
     deck.push([name_image,j]);
   }
 }
 var dealer=function(){
+  let tmp2=[];
+  flag_end=1;
   if (endgame==0){
     for (let i=0; i<6; i++){
       if (scoredealer<scoreplayer && scoredealer<=42) {
-        scoredealer = deal(scoredealer, "Dealer's","dealerarea");
+        tmp2 = deal(scoredealer, "Dealer's","dealerarea",as_dealer);
+        scoredealer=tmp2[0];
+        as_dealer=tmp2[1];
+        if (scoredealer>42 && as_dealer>0) {
+          scoredealer-=10;
+          as_dealer-=1;
+          // console.log(tmp2);
+          tmp2 = deal(scoredealer, "Dealer's","dealerarea",as_dealer);
+          // console.log(tmp2);
+          scoredealer=tmp2[0];
+          as_dealer=tmp2[1];
+          document.getElementById("Dealer's").innerHTML = "Dealer's score : "+ scoredealer;
+        }
       }
       endgame=1;
     }
@@ -41,18 +61,32 @@ var dealer=function(){
 }
 var resultats=function(){
   if ((scoreplayer<=42 && scoredealer<scoreplayer) || (scoreplayer<=42 && scoredealer>42)){
-    window.alert("Le joueur a un score de " + scoreplayer + "La banque a un score de "+scoredealer+" Le joueur a gagné !");
+    window.alert("Le joueur a un score de " + scoreplayer + "\nLa banque a un score de "+scoredealer+"\nLe joueur a gagné !");
   }
   else {
-    window.alert("Le joueur a un score de " + scoreplayer + "La banque a un score de "+scoredealer+" Le joueur a perdu !");
+    window.alert("Le joueur a un score de " + scoreplayer + "\nLa banque a un score de "+scoredealer+"\nLe joueur a perdu !");
   }
 }
 var player=function(){
-  if (scoreplayer<=42){
-    scoreplayer = deal(scoreplayer,"Your","yourarea");
+  let tmp=[];
+  if (flag_end==0){
+    if (scoreplayer<=42){
+      tmp = deal(scoreplayer,"Your","yourarea",as_player);
+      scoreplayer=tmp[0];
+      as_player=tmp[1];
+      if (scoreplayer>42 && as_player>0){
+        scoreplayer-=10;
+        as_player-=1;
+        document.getElementById("Your").innerHTML = "Your score : "+ scoreplayer;
+      }
+    }
+    if (scoreplayer>42){
+      window.alert("GAME OVER !");
+    }
   }
-  if (scoreplayer>42){
-    window.alert("GAME OVER !");
+  else {
+    window.alert ("La partie est déjà terminée !");
+    resultats();
   }
 }
 
@@ -71,18 +105,24 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-var deal=function(score,challenger,area){
+var deal=function(score,challenger,area,count_as){
   let random_index=getRandomInt(nbcard);
   addImgInDiv(deck[random_index][0],area);
+  if (deck[random_index][1]==11){
+    count_as++;
+  }
   score+=deck[random_index][1];
   deck.splice(random_index,1);
   nbcard--;
   document.getElementById(challenger).innerHTML = challenger+" score : "+ score;
-  return score;
+  return [score,count_as];
   // console.log(random_index);
   // console.log(deck);
 }
 
+let as_player=0;
+let as_dealer=0;
+let flag_end=0;
 let endgame=0;
 let deck=Array();
 let nbcard=52;
